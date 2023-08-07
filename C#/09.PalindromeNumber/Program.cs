4ebe0f9ac9f9b2151b58fc429dbbf8a9dcfa58a0
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-08-07 11:26:07 -0500

    9. Palindrome Number C#

---
+++ C#/09.PalindromeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.IsPalindrome(12321));// expected true ✔️
+        Console.WriteLine(Solution.IsPalindrome(-121)); // expected false ✔️
+        Console.WriteLine(Solution.IsPalindrome(10)); // expected false ✔️
+        Console.WriteLine(Solution.IsPalindrome(12321)); // expected true ✔️
+    }
+}
+
+
+class Solution
+{
+    public static bool IsPalindrome(int x)
+    {
+        var stringNumber = x.ToString();
+        var StringReversed = stringNumber.ToCharArray();
+        Array.Reverse(StringReversed);
+
+        for (int i = 0; i < StringReversed.Length; i++)
+        {
+            if (StringReversed[i] != stringNumber[i])
+            {
+                return false;
+            }
+        }    
+
+        return true;    
+    }
+}
