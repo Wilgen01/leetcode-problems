d5deaf5471abb4bf08cdc8558e97b784d6d5a1ee
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-08-14 14:30:58 -0500

    14.LongestCommonPrefix C#

---
+++ C#/14.LongestCommonPrefix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.LongestCommonPrefix(new string[] {"flower","flow","flight"})); // expected "fl" ✔️
+        Console.WriteLine(Solution.LongestCommonPrefix(new string[] {"dog","racecar","car"})); // expected "" ✔️
+        Console.WriteLine(Solution.LongestCommonPrefix(new string[] {"flower","flower", "flo"})); // expected "" ✔️
+    }
+}
+
+public class Solution {
+    public static string LongestCommonPrefix(string[] strs) {
+        for (int i = 0; i < strs[0].Length; i++) {
+            var letter = strs[0][i];
+            for (int j = 0; j < strs.Length; j++)
+            {
+                var element = strs[j];
+                if (element.Length <= i || element[i] != letter)
+                {
+                    return strs[0].Substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
