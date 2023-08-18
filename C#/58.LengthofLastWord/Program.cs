4d116013ea7ae303fdfd97a55d3eb21dc0dc3073
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-08-17 20:33:13 -0500

    58.LengthofLastWord C#

---
+++ C#/58.LengthofLastWord/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.LengthOfLastWord("Hello World")); // expected 5 ✔️
+        Console.WriteLine(Solution.LengthOfLastWord("   fly me   to   the moon  ")); // expected 4 ✔️
+        Console.WriteLine(Solution.LengthOfLastWord("luffy is still joyboy")); // expected 6 ✔️
+    }
+}
+
+public class Solution {
+    public static int LengthOfLastWord(string s) {
+        var wordArr = s.Trim().Split(" ");
+        return wordArr[wordArr.Length - 1].Length;
+    }
+}
